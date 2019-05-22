3a8d0f83116399ee1ff7408ad491bea7de5c0080
acosta <alexmcosta.dev@gmail.com>
2019-05-22 09:37:44 -0700

    Passing server routes

--- server.go
+++ server.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Hello World!</h1>")
+}
+func projects(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Projects</h1>")
 }
 
 func main() {
-
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/projects", projects)
+	http.ListenAndServe(":3000", r)
 }
--- server_test.go
+++ server_test.go
@@ -7,14 +7,28 @@ import (
 )
 
 func TestGETPage(t *testing.T) {
-	t.Run("Returns the home page", func(t *testing.T) {
+	t.Run("Returns the Home page", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
 
-		handler(response, request)
+		home(response, request)
 
 		got := response.Body.String()
 		want := "<h1>Hello World!</h1>"
+
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
+
+	t.Run("Returns the Projects page", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/projects", nil)
+		response := httptest.NewRecorder()
+
+		projects(response, request)
+
+		got := response.Body.String()
+		want := "<h1>Projects</h1>"
 
 		if got != want {
 			t.Errorf("got '%s', want '%s'", got, want)
